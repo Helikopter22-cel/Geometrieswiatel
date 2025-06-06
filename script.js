f5e0636652a04555eef15f1082717ec16fdff74b
Helikopter22-cel <helena.nowakowska2000@gmail.com>
2025-06-06 17:34:36 +0200

    script.js

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ function openModal(index, title, description) {
     document.getElementById('modal-title').innerText = title;
     document.getElementById('modal-desc').innerText = description;
 }
+<script>
+function openModal(imageSrc, captionText) {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImage");
+  const caption = document.getElementById("modalCaption");
+  
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+  caption.innerText = captionText;
+}
+
+function closeModal() {
+  document.getElementById("imageModal").style.display = "none";
+}
+</script>
 
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
